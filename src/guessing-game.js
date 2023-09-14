eaf004305b9d9d0e0a42e26e5c653b5a461da7d0
Alex_Nout <calium_by@mail.ru>
2023-09-14 17:54:51 +0300

    done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
     constructor() {}
 
+    left;
+    right;
+    result;
     setRange(min, max) {
-
+        this.left = min;
+        this.right = max;
     }
 
     guess() {
-
+        this.result = Math.ceil((this.right + this.left)/2);
+        return this.result
     }
 
     lower() {
-
+        this.right = this.result;
     }
 
     greater() {
-
+        this.left = this.result;
     }
 }
 
